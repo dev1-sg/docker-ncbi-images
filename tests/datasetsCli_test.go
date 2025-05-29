3a6a3b115bc66d118303c6abe3b3d529081ad711
antyung47 <antyung47@gmail.com>
2025-05-30 02:50:11 +0800

    Add testcontainers

---
+++ tests/datasetsCli_test.go
@@ -0,0 +1,81 @@
+package tests
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+var datasetsCli = struct {
+	AWS_DEFAULT_REGION string
+	AWS_ECR_PUBLIC_URI string
+	DOCKER_IMAGE_GROUP string
+	DOCKER_IMAGE       string
+	DOCKER_IMAGE_TAG   string
+}{
+	AWS_DEFAULT_REGION: "us-east-1",
+	AWS_ECR_PUBLIC_URI: "public.ecr.aws/dev1-sg",
+	DOCKER_IMAGE_GROUP: "ncbi",
+	DOCKER_IMAGE:       "datasets-cli",
+	DOCKER_IMAGE_TAG:   "latest",
+}
+
+func TestContainersGoPulldatasetsCli(t *testing.T) {
+	ctx := context.Background()
+	for attempt := 0; attempt < 3; attempt++ {
+		container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: testcontainers.ContainerRequest{
+				Image: datasetsCli.AWS_ECR_PUBLIC_URI + "/" + datasetsCli.DOCKER_IMAGE_GROUP + "/" + datasetsCli.DOCKER_IMAGE + ":" + datasetsCli.DOCKER_IMAGE_TAG,
+			},
+		})
+		require.NoError(t, e)
+		container.Terminate(ctx)
+	}
+}
+
+func TestContainersGoExecdatasetsCliDatasets(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: datasetsCli.AWS_ECR_PUBLIC_URI + "/" + datasetsCli.DOCKER_IMAGE_GROUP + "/" + datasetsCli.DOCKER_IMAGE + ":" + datasetsCli.DOCKER_IMAGE_TAG,
+			Cmd:   []string{"sleep", "10"},
+		},
+		Started: true,
+	})
+	require.NoError(t, e)
+	defer container.Terminate(ctx)
+
+	exitCode, reader, e := container.Exec(ctx, []string{"datasets", "--version"})
+	require.NoError(t, e)
+	require.Equal(t, 0, exitCode)
+
+	output, e := io.ReadAll(reader)
+	require.NoError(t, e)
+
+	require.Contains(t, string(output), "datasets", "Expected output not found")
+}
+
+func TestContainersGoExecdatasetsCliDataformat(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: datasetsCli.AWS_ECR_PUBLIC_URI + "/" + datasetsCli.DOCKER_IMAGE_GROUP + "/" + datasetsCli.DOCKER_IMAGE + ":" + datasetsCli.DOCKER_IMAGE_TAG,
+			Cmd:   []string{"sleep", "10"},
+		},
+		Started: true,
+	})
+	require.NoError(t, e)
+	defer container.Terminate(ctx)
+
+	exitCode, reader, e := container.Exec(ctx, []string{"dataformat", "version"})
+	require.NoError(t, e)
+	require.Equal(t, 0, exitCode)
+
+	output, e := io.ReadAll(reader)
+	require.NoError(t, e)
+
+	require.Contains(t, string(output), "undefined", "Expected output not found")
+}
